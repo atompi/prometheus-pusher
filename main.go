5528133a3a8c3a155e055e955ccbe6047f8a8b6f
atompi <atompi@foxmail.com>
2018-08-22 19:22:56 +0800

    make it runnable

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"github.com/mission802/prometheus-pusher/scrape"
 	"github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/retrieval"
 	"github.com/prometheus/prometheus/storage"
-	"github.com/yunlzheng/prometheus-pusher/scrape"
 	"strings"
 )
 
